323ebb1bad3d67ef70e875c6e954d3ea892673fa
akar995 <akar.imdad1995@gmail.com>
2025-09-19 20:59:58 +0300

    build for web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "933220839"
+    serviceWorkerVersion: "3782852725"
   }
 });
